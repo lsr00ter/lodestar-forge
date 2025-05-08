bd251f7b2301059ff992817a21bf2d86bad4c354
Jamie Pegg <jamiepegg@zoho.com>
2025-05-08 16:19:31 +0000

    Add height constraints to project table

--- command/src/components/projects/all-projects.jsx
+++ command/src/components/projects/all-projects.jsx
@@ -60,7 +60,7 @@ export default function AllProjects({ projects }) {
                     value={searchValue}
                     onChange={(e) => setSearchValue(e.target.value)}
                 />
-                <ScrollArea className="h-[calc(100vh-450px)]">
+                <ScrollArea className="min-h-[calc(100vh-700px)] max-h-[calc(100vh-450px)]">
                     {filteredProjects.length ? (
                         <div className="flex flex-col gap-2">
                             {filteredProjects.map((project) => (
@@ -71,7 +71,7 @@ export default function AllProjects({ projects }) {
                             ))}
                         </div>
                     ) : (
-                        <div className="flex rounded-md h-[calc(100vh-450px)] w-full items-center justify-center border border-dashed">
+                        <div className="flex rounded-md min-h-[calc(100vh-700px)] max-h-[calc(100vh-450px)] w-full items-center justify-center border border-dashed">
                             <p className="text-sm text-muted-foreground">
                                 No projects found.
                             </p>
