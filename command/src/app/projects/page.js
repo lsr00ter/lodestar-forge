c94466c32daade13f43f15f73587a1fd5c7880f8
Jamie <38860802+c0nf1den71al@users.noreply.github.com>
2025-05-11 08:10:14 +0100

    Remove old comments

--- command/src/app/projects/page.js
+++ command/src/app/projects/page.js
@@ -3,7 +3,6 @@ import { apiFetch } from "@/lib/utils";
 
 export default async function Projects() {
     const rows = await apiFetch("/projects");
-    // TODO: Make this page have a search bar and also hide "done" projects.
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
             <AllProjects projects={rows} />
