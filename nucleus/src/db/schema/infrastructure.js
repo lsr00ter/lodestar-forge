24c9d9eda6703868ce315667c8ec706d55047bf0
Jamie Pegg <jamie@development.jamiepegg.com>
2025-08-01 17:50:22 +0100

    Removed unused imports

--- nucleus/src/db/schema/infrastructure.js
+++ nucleus/src/db/schema/infrastructure.js
@@ -1,30 +1,29 @@
 import { pgTable, text, pgEnum, json } from "drizzle-orm/pg-core";
 import crypto from "crypto";
 import { deployments } from "./deployments.js";
-import { templates } from "./templates.js";
 
 export const infrastructureStatusEnum = pgEnum("infrastructureStatus", [
-  "pending",
-  "building",
-  "configuring",
-  "default",
-  "failed",
-  "running",
-  "destroyed",
+    "pending",
+    "building",
+    "configuring",
+    "default",
+    "failed",
+    "running",
+    "destroyed",
 ]);
 
 export const infrastructure = pgTable("infrastructure", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  deploymentId: text("deploymentId").references(() => deployments.id, {
-    onDelete: "cascade",
-  }),
-  name: text("name").notNull(),
-  template: json("template"),
-  configurations: json("configurations").array(),
-  deployedConfigurations: json("deployedConfigurations").array(),
-  status: infrastructureStatusEnum("status").notNull().default("pending"),
-  description: text("description"),
-  username: text("username"),
+    id: text("id")
+        .primaryKey()
+        .$defaultFn(() => crypto.randomUUID()),
+    deploymentId: text("deploymentId").references(() => deployments.id, {
+        onDelete: "cascade",
+    }),
+    name: text("name").notNull(),
+    template: json("template"),
+    configurations: json("configurations").array(),
+    deployedConfigurations: json("deployedConfigurations").array(),
+    status: infrastructureStatusEnum("status").notNull().default("pending"),
+    description: text("description"),
+    username: text("username"),
 });
